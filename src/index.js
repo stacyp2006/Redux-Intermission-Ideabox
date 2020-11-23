fe63190b537b9b17caf400416c48c0eb8dbfc3fa
Stacy Potten <stacylynnaz@gmail.com>
2020-11-23 14:55:41 -0700

    Add imports to index.js to create store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
+
+
 
 ReactDOM.render(
   <App />,
