faae206c1602d0143ce050505cbcbebf9553097b
Stacy Potten <stacylynnaz@gmail.com>
2020-11-23 20:09:42 -0700

    Update todo list to display the todos

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import ToDo from '../components/ToDo';
+import { connect } from 'react-redux';
 
-const ToDoList = ({todos}) => {
-  console.log(todos);
+const ToDoList = ({ todos }) => {
+  const displayTodos = todos.map(todo => {
+    return (
+      <ToDo
+        {...todo}
+        key={todo.id}
+      />
+    )
+  })
 
   return (
     <ul>
+      {displayTodos}
     </ul>
   )
 }
 
 const mapStateToProps = state => ({
-  todos: state.ToDoList
+  todos: state.todos
 });
 
 export default connect(mapStateToProps)(ToDoList);
