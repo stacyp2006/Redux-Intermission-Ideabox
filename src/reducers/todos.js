d2dbc01583e612007ede780f2e6c304dca5396f9
Stacy Potten <stacylynnaz@gmail.com>
2020-11-24 10:59:04 -0700

    Add completeTodo case to reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,6 +2,13 @@ export const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [...state, { id: Date.now(), todo: action.todo, completed: false}];
+    case 'COMPLETE_TODO':
+      return state.map(todo => {
+        if (action.id === todo.id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
     default:
       return state;
   }
